b126cd9a331c4cd1a128f8c8714dc3fc3f3ae85e
Prateek  Nayak <prateek.nayak1708@gmail.com>
2015-12-24 07:12:44 +1100

    One script to run them all... do you even reference bro ?

--- elk.sh
+++ elk.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
 
+echo "Installing elk (elastic kibana logstash nginx)"
+
+sh ./installs/java.sh 2&1> javainstall.log
+
+echo "Java install process finished check javainstall.log for details"
+
+echo "Starting elastic search install"
 
 
